c2d1253beaaf5f3d908c9016ce254d02c6525014
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2020-05-15 16:27:51 -0400

    ola's changes

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,22 @@
+require "pry"
+require "./startup.rb"
+require "./venture_capitalist.rb"
+
 class FundingRound
+    attr_accessor :type
+    attr_reader :startup, :venture_capitalist 
+    @@all = []
+    def initialize(startup, venture_capitalist, type, investment)
+        @investment = investment.to_f
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @type = type
+        FundingRound.all << self
+    end     
+
+    def self.all
+        @@all
+    end
 
 end
+binding.pry
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,17 +1,33 @@
-require 'pry'
+#  require 'pry'
 class Startup
   attr_reader :name, :founder, :domain
+  @@all = []
   def initialize(name, founder, domain)
     #start up name
     @name = name
     @founder = founder
     @domain = domain
+    @@all << self
   end
 
   def pivot(domain, name)
     @domain = domain
     @name = name
   end
+  def self.all
+    @@all
+  end
+  def self.find_by_founder(name)
+    self.all.find do |startup|
+      startup.founder == name
+    end
+  end
+  def self.domains
+    self.all.map do |startup|
+      startup.domain
+    end
+  end
+
 end
-binding.pry
+#  binding.pry
  
