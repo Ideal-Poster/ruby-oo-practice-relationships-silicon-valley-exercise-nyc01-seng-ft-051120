056dcf640e6cd4748c5928f9c1abbc60143d8a3d
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-05-15 15:15:27 -0400

    progress

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Startup
+  attr_reader :name, :founder, :domain
+  def initialize(name, founder, domain)
+    #start up name
+    @name = name
+    @founder = founder
+    @domain = domain
+  end
 
+  def pivot(domain, name)
+    @domain = domain
+    @name = name
+  end
 end
+binding.pry
+ 
